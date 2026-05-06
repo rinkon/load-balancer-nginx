c76a9b2a0a3cfa5bf440e3da05174b29cbc79635
Ashik Aowal <r.aowal@gmail.com>
2026-05-06 10:17:34 +1000

    docker compose with nginx load balancer conf added

--- app/main.py
+++ app/main.py
@@ -3,9 +3,12 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/hello')
 def hello(request: Request):
-    server_no = request.url.port - 8000 + 1
+    port = request.scope.get("server")[1]
+    server_no = port - 8000 + 1
+    # server_no = request.url.port - 8000 + 1
+
     return {
         "server_no": server_no
     }
